fd456e9c581546feb014ba3772a3dab5d2521da7
Maeinukhan <khanmaeinuddin19@gmail.com>
2024-07-30 01:07:02 +0530

    third try

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,5 +1,5 @@
 package com.driver;
-
+// package com.driver.Meeting;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
@@ -47,21 +47,21 @@ public class Workspace extends Gmail{
         return count;
     }
 
-    public class Meeting {
-        private LocalTime startTime;
-        private LocalTime endTime;
+    // public class Meeting {
+    //     private LocalTime startTime;
+    //     private LocalTime endTime;
 
-        public Meeting(LocalTime startTime, LocalTime endTime) {
-            this.startTime = startTime;
-            this.endTime = endTime;
-        }
+    //     public Meeting(LocalTime startTime, LocalTime endTime) {
+    //         this.startTime = startTime;
+    //         this.endTime = endTime;
+    //     }
 
-        public LocalTime getStartTime() {
-            return startTime;
-        }
+    //     public LocalTime getStartTime() {
+    //         return startTime;
+    //     }
 
-        public LocalTime getEndTime() {
-            return endTime;
-        }
-    }
+    //     public LocalTime getEndTime() {
+    //         return endTime;
+    //     }
+    // }
 }
